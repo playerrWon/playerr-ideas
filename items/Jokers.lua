bc7b3d67322b2fd3c05d2327ff4b7c1770168ef0
playerrWon <smileymathvideos@gmail.com>
2025-05-07 13:00:37 -0400

    Update Jokers.lua

--- items/Jokers.lua
+++ items/Jokers.lua
@@ -1,31 +1,31 @@
 SMODS.Joker{
-key = "lucky7s",
-pos = {x = 0, y = 0},
-rarity = 3,
-atlas = "PLH",
-config = { extra = {X_mult = 1, mult_mod = 0.1 } },
-cost = 6,
-loc_txt =
-	name = "Lucky 7s"
-	text = {
-		"can't bother to work out text"
-	}	 
-if card.lucky-trigger then 
-return {Xmult_mod = card.ability.extra.X_mult}
-        end,
-         calculate = function (self, card, context)
-            if context.individual and context.cardarea == G.play and (context.other_card:get_id() == 7) then
- return { 
-                    x_mult = card.ability.extra.x_mult,
-					colour = G.C.RED,
-					card = card, 
-			}
-                end
-            end,
+    key = "lucky7s",
+    pos = {x = 0, y = 0},
+    rarity = 3,
+    atlas = "PLH",
+    config = { extra = { X_mult = 1, mult_mod = 0.1 } },
+    cost = 6,
+    loc_txt = {
+        name = "Lucky 7s",
+        text = {
+            "can't bother to work out text"
+        }
+    },
+    loc_def = function(self, card)
+        if card["lucky_trigger"] then
+            return { Xmult_mod = card.ability.extra.X_mult }
+        end
+    end,
+    calculate = function(self, card, context)
+        if context.individual and context.cardarea == G.play and (context.other_card:get_id() == 7) then
+            return {
+                x_mult = card.ability.extra.X_mult,
+                colour = G.C.RED,
+                card = card,
+            }
+        end
+    end,
 }
-
-        
-
 
 
             
