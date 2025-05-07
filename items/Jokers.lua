a22117a71c8cb48ca6a7227572e1a913be8c07bb
playerrWon <smileymathvideos@gmail.com>
2025-05-07 15:18:44 -0400

    Update Jokers.lua

--- items/Jokers.lua
+++ items/Jokers.lua
@@ -1,23 +1,27 @@
-SMODS.Joker{
-    key = "lucky7s",
+SMODS.Joker {
+    key = "Lucky7's",
     pos = {x = 0, y = 0},
     rarity = 3,
     atlas = "PLH",
     config = { extra = { X_mult = 1, mult_mod = 0.1 } },
     cost = 6,
     loc_txt = {
-        name = "Lucky 7s",
+        name = "Lucky 7's",
         text = {
-            "can't bother to work out text"
+            "insert text here"
         }
-    }
+    },
+    loc_vars = function (self, info_queue, center)
+        return { vars = { center.ability.extra.X_mult, center.ability.extra.mult_mod } }
+    end,
     calculate = function (self, card, context)
-if context.individual and context.other_card.lucky_trigger then
-    card.ability.extra.X_mult = card.ability.extra.X_mult + card.ability.extra.mult_mod
-end
-if context.individual and context.cardarea == G.play then
-    if context.other_card.base.nominal == 7 then
-        return {xmult = card.ability.extra.X_mult}
-    end
-        end,
-    } 
+        if context.individual and context.other_card.lucky_trigger then
+            card.ability.extra.X_mult = card.ability.extra.X_mult + card.ability.extra.mult_mod
+        end
+        if context.individual and context.cardarea == G.play then
+            if context.other_card.base.nominal == 7 then
+                return {xmult = card.ability.extra.X_mult}
+            end
+        end
+    end,
+}
